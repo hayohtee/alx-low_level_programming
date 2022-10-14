5584621abecec81db20b45b867cf90ba3f21c1d4
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2022-10-14 05:59:33 -0700

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - The entry point for the program
+  * Return: The exit code
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+		putchar('z' - i);
+	putchar('\n');
+
+	return (0);
+}
