6d9473837e51e42beae95996f641ecc4dd82b0be
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2022-10-14 06:23:33 -0700

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,18 +10,14 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		if (i == 9)
+		putchar('0' + i);
+		if (i < 9)
 		{
-			putchar('0' + i);
-		}
-		else
-		{
-			putchar('0' + i);
 			putchar(',');
 			putchar(' ');
 		}
 	}
-
+	putchar('\n');
 
 	return (0);
 }
