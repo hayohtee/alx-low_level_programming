d9b48011cf29d7ff75181b6aae22d262b46732d8
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2022-10-18 03:11:43 -0700

    Create some program

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add - adds two number
+* @x : the first number
+* @y : the second number
+* Return: the sum of x and y
+*/
+
+int add(int x, int y)
+{
+	return (x + y);
+}
