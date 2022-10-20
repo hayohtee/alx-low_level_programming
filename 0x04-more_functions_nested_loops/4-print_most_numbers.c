9add5f7f576f264e115338aba13b67c4b60b2226
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2022-10-20 14:39:39 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - Print numbers from 0 - 9 excluding 2 and 4
+  */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 || 1 != 4)
+			_putchar('0' + i);
+	}
+	_putchar('\n');
+}
