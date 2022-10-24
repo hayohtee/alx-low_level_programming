e11fcb95fb5fa10e0f78225d0f2e276e938909d8
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2022-10-24 11:51:51 +0100

    Update file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
-* reset_to_98 - update the value of an integer to 98
-* @n: the address of the integer
-*/
+ * reset_to_98 - sets the value of the pointer to 98
+ * @n: int type pointer
+ *
+ * Return: void
+ */
+
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
