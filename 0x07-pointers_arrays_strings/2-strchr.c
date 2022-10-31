0f74b14c0618edce29ded390a08cbe35bc72bd1b
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2022-10-31 12:55:05 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,11 +10,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	for (; *s != '\0'; s++)
 	{
-		if (*s == c)
-			return (s);
-		s++;
+	if (*s == c)
+		return (s);
 	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	else
 	return (NULL);
 }
